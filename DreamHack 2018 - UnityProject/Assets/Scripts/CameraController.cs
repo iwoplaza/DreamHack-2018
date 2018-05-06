f79b8c8c67ecae1b34c248f6648e7d5e7d73df61
Daniel Munch <daniellmunch@gmail.com>
2018-05-06 17:41:41 +0200

    Fixed bug with deselecting when click on UI
    The buttons still doesn't work for some reason

--- DreamHack 2018 - UnityProject/Assets/Scripts/CameraController.cs
+++ DreamHack 2018 - UnityProject/Assets/Scripts/CameraController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.EventSystems;
 
 namespace Game
 {
@@ -57,7 +58,7 @@ namespace Game
             float vertical = CrossPlatformInputManager.GetAxis("Mouse Y");
             float scrollWheel = CrossPlatformInputManager.GetAxis("Mouse ScrollWheel");
 
-            if(Input.GetMouseButtonDown(0))
+            if(!EventSystem.current.IsPointerOverGameObject() && Input.GetMouseButtonDown(0))
             {
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hitInfo;
