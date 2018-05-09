40ff43a0308b05e511769af516d29523a58f9c6b
Anthony M <anthonyme1211@gmail.com>
2018-05-09 19:09:00 +0700

    -Updated a few models -Created a base class for animated objects

---
+++ DreamHack 2018 - UnityProject/Assets/Scripts/AnimatedObjects/AnimatedComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+	public abstract class AnimatedComponent : MonoBehaviour {
+
+		public AnimatedObject Owner { get; private set; }
+
+		public void RegisterComponent(AnimatedObject newOwner)
+		{
+			Owner = newOwner;
+			Owner.RegisterListener(AnimatedObject.ListenerType.ON_ACTIVATE, OnActivate);
+			Owner.RegisterListener(AnimatedObject.ListenerType.ON_DEACTIVATE, OnDeactivate);
+		}
+
+		public abstract void OnActivate();
+
+		public abstract void OnDeactivate();
+	}
+}
