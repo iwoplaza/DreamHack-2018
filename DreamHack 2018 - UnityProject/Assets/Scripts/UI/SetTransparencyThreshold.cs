fac09b2d7e39f9ec47d80180a09a60e945961495
Anthony M <anthonyme1211@gmail.com>
2018-05-09 15:12:37 +0700

    -Set the UI prefab to have an alpha threshold for hit detection. -Base model for vegetation

---
+++ DreamHack 2018 - UnityProject/Assets/Scripts/UI/SetTransparencyThreshold.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class SetTransparencyThreshold : MonoBehaviour {
+
+	Image m_Image;
+
+	// Use this for initialization
+	void Start () {
+		m_Image = GetComponent<Image>();
+		m_Image.alphaHitTestMinimumThreshold = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
