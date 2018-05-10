26ae42323d56d1237ddfd128b5ffaf1e56e3ae0d
Iwo Plaza <gobbobminecraft@gmail.com>
2018-05-10 14:54:10 +0200

    Working on the Build Menu.

---
+++ DreamHack 2018 - UnityProject/Assets/Scripts/UI/BuildCataloguePanel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.UI
+{
+    public class BuildCataloguePanel : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            gameObject.SetActive(false);
+            WorldController.Instance.RegisterModeChangeHandler(OnModeChange);
+        }
+
+        void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnModeChange(PlayMode newMode)
+        {
+            if(newMode == PlayMode.BUILD_MODE)
+            {
+                Show();
+            }
+            else
+            {
+                Hide();
+            }
+        }
+    }
+}
