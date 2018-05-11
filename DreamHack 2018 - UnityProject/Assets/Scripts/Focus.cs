d8c0ca1233302c3d7c7f80cad97999d83427dc00
Iwo Plaza <gobbobminecraft@gmail.com>
2018-05-11 10:55:01 +0200

    Added a working dynamicly renderer portrait for FocusTargets.

--- DreamHack 2018 - UnityProject/Assets/Scripts/Focus.cs
+++ DreamHack 2018 - UnityProject/Assets/Scripts/Focus.cs
@@ -8,9 +8,20 @@ namespace Game
     {
         public delegate void FocusHandler(IFocusTarget focusTarget);
         public IFocusTarget Current { get; private set; }
+        public Camera PortraitCamera { get; private set; }
+        public RenderTexture PortraitTexture { get; private set; }
 
         private FocusHandler m_focusGainHandlers;
         private FocusHandler m_focusLossHandlers;
+
+        public Focus()
+        {
+            GameObject gameObject = new GameObject("PortraitCamera");
+            PortraitCamera = gameObject.AddComponent<Camera>();
+            PortraitTexture = new RenderTexture(256, 256, 24);
+            PortraitCamera.targetTexture = PortraitTexture;
+            PortraitCamera.fieldOfView = 22.0F;
+        }
 
         public void On(IFocusTarget newTarget)
         {
@@ -25,6 +36,14 @@ namespace Game
             if (newTarget != null)
             {
                 Current.OnFocusGained();
+
+                if (newTarget.PortraitPivot != null)
+                {
+                    PortraitCamera.transform.parent = newTarget.PortraitPivot;
+                    PortraitCamera.transform.localRotation = Quaternion.Euler(0, -150, 0);
+                    PortraitCamera.transform.localPosition = new Vector3(0.553F, 0.661F, 0.969F);
+                }
+
                 if (m_focusGainHandlers != null)
                     m_focusGainHandlers(Current);
             }
