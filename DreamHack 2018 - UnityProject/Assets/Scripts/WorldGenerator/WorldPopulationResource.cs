50f793c37ec02af37f9490057301d00cab4c50d6
Anthony M <anthonyme1211@gmail.com>
2018-05-12 22:29:49 +0700

    -Added plant prefabs

--- DreamHack 2018 - UnityProject/Assets/Scripts/WorldGenerator/WorldPopulationResource.cs
+++ DreamHack 2018 - UnityProject/Assets/Scripts/WorldGenerator/WorldPopulationResource.cs
@@ -9,18 +9,21 @@ namespace Game.Environment
         public enum PopulationType
         {
             VEGETATION_DESERT,
+            VEGETATION_GREEN,
             ROCK_SMALL,
             ROCK_LARGE
         }
 
         private static List<GameObject> m_desertVegetation;
+        private static List<GameObject> m_greenVegetation;        
         private static List<GameObject> m_RocksSmall;
         private static List<GameObject> m_RocksLarge;
 
 
         public static void UpdatePopulationDirectory()
         {
-            m_desertVegetation = new List<GameObject>();            
+            m_desertVegetation = new List<GameObject>();
+            m_greenVegetation = new List<GameObject>();         
             m_RocksSmall = new List<GameObject>();            
             m_RocksLarge = new List<GameObject>();            
 
@@ -57,6 +60,17 @@ namespace Game.Environment
                     }
                 }
             }
+            for(int i = 0; i < 99; i++)
+            {                
+                if(Resources.TileObjectPrefabs.ContainsKey("PlantGreen_" + i.ToString()))
+                {     
+                    GameObject temp = Resources.TileObjectPrefabs.Find("PlantGreen_" + i.ToString());
+                    if(temp != null)
+                    {
+                        m_greenVegetation.Add(temp);
+                    }
+                }
+            }
         }
 
         public static GameObject GetResources(PopulationType type)
@@ -65,11 +79,12 @@ namespace Game.Environment
             {
                 case PopulationType.VEGETATION_DESERT:
                     return m_desertVegetation[Random.Range(0,m_desertVegetation.Count)];
+                case PopulationType.VEGETATION_GREEN:
+                    return m_greenVegetation[Random.Range(0,m_greenVegetation.Count)];
                 case PopulationType.ROCK_SMALL:
                     return m_RocksSmall[Random.Range(0,m_RocksSmall.Count)];
                 case PopulationType.ROCK_LARGE:
-                    return m_RocksLarge[Random.Range(0,m_RocksLarge.Count)];
-
+                    return m_RocksLarge[Random.Range(0,m_RocksLarge.Count)];                
             }
             return null;
         }
